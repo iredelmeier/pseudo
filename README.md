cbce35c38b9e908d02ae13435279a29efa25670b
Isobel Redelmeier <iredelmeier@gmail.com>
2017-03-15 17:07:17 -0700

    Expand README and add examples

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Pseudo
+
+### A small mocking library for Rust.
+
+Pseudo lets you mock `Trait` implementations so that you can track function call arguments and set return values or overrides functions at test time.
+
+Here's a quick example:
+
+```rust
+extern crate pseudo;
+
+use pseudo::Mock;
+
+trait Foo: Clone {
+    fn expensive_fn(&self, x: i64, y: i64) -> i64;
+}
+
+#[derive(Clone)]
+struct MockFoo {
+    pub expensive_fn: Mock<(i64, i64), i64>,
+}
+
+impl Foo for MockFoo {
+    fn expensive_fn(&self, x: i64, y: i64) -> i64 {
+        self.expensive_fn.call((x + 10, y))
+    }
+}
+
+fn double_expensive_fn<T: Foo>(foo: &T, x: i64, y: i64) -> i64 {
+    foo.expensive_fn(x, y) * 2
+}
+
+#[test]
+fn doubles_return_value() {
+    let mock = MockFoo { expensive_fn: Mock::default() };
+
+    mock.expensive_fn.return_value(1000);
+
+    assert_eq!(double_expensive_fn(&mock, 1, 2), 2000);
+}
+
+#[test]
+fn uses_correct_args() {
+    let mock = MockFoo { expensive_fn: Mock::default() };
+
+    assert!(!mock.expensive_fn.called());
+
+    double_expensive_fn(&mock, 1, 2);
+
+    assert_eq!(mock.expensive_fn.num_calls(), 1);
+    assert!(mock.expensive_fn.called_with((11, 2)));
+}
+```
+
+More examples are available in the [examples directory](./examples).
--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,55 @@
-// Pseudo is a small library for making mock `Trait` implementations.
-//
-// The `Mock` struct tracks function call arguments and specifies return
-// values or function overrides.
-//
-// # Examples
-//
-// Using `Mock` for testing:
-//
-// ```
-// use subtitute::Mock;
-//
-// trait Foo {
-//     fn test_fn(&self, arg: i64) -> i64;
-// }
-//
-// struct MockFoo {
-//     pub test_fn: Mock<i64, i64>,
-// }
-//
-// impl Foo for MockFoo {
-//     fn test_fn(&self, arg: i64) -> i64 {
-//         self.test_fn.call(arg)
-//     }
-// }
-//
-// impl Default for MockFoo {
-//     fn default() -> Self {
-//         MockFoo { test_fn: Mock::default() }
-//     }
-// }
-//
-// fn fn_using_foo_trait<T: Foo>(x: i64, y: i64, foo: T) -> i64 {
-//     foo.test_fn(x + y)
-// }
-//
-// struct StructUsingFoo<T: Foo> {
-//     foo: T,
-// }
-//
-// impl<T: Foo> StructUsingFoo
-//
-// let mock = MockFoo::default();
-//
-// ```
-//
-// Using `Mock` for functions with various signature types:
-//
-// ```
-// use pseudo::Mock;
-//
-// trait Foo {
-//     fn simple_fn(&self, arg: String) -> String;
-//     fn void_fn(&self, arg: String);
-//     fn zero_arg_fn(&self) -> String;
-//     fn multi_arg_fn(&self, x: i64, y: bool) -> bool;
-// }
-//
-// struct MockFoo {
-//     pub simple_fn: Mock<String, String>,
-//     pub void_fn: Mock<String, ()>,
-//     pub zero_arg_fn: Mock<(), String>,
-//     pub multi_arg_fn: Mock<(i64, bool), bool>
-// }
-//
-// impl Default for MockFoo {
-//     fn default() -> Self {
-//         MockFoo {
-//             simple_fn: Mock::default(),
-//             void_fn: Mock::default(),
-//             zero_arg_fn: Mock::default(),
-//             multi_arg_fn: Mock::default(),
-//         }
-//     }
-// }
-//
-// impl Foo for MockFoo {
-//     fn simple_fn(&self, arg: String) -> String {
-//         self.simple_fn.call(arg)
-//     }
-//
-//     fn void_fn(&self, arg: String) {
-//         self.void_fn.call(arg)
-//     }
-//
-//     fn zero_arg_fn(&self) -> String {
-//         self.zero_arg_fn.call(())
-//     }
-//
-//     fn multi_arg_fn(&self, x: i64, y: bool) -> bool {
-//         self.multi_arg_fn.call((x, y))
-//     }
-// }
-// ```
+//! Pseudo is a small library for mocking `Trait` implementations.
+//!
+//! The `Mock` struct tracks function call arguments and specifies return
+//! values or function overrides.
+//!
+//! # Examples
+//!
+//! ```
+//! use pseudo::Mock;
+//!
+//! trait Foo: Clone {
+//!     fn expensive_fn(&self, x: i64, y: i64) -> i64;
+//! }
+//!
+//! #[derive(Clone)]
+//! struct MockFoo {
+//!     pub expensive_fn: Mock<(i64, i64), i64>,
+//! }
+//!
+//! impl Foo for MockFoo {
+//!     fn expensive_fn(&self, x: i64, y: i64) -> i64 {
+//!         self.expensive_fn.call((x + 10, y))
+//!     }
+//! }
+//!
+//! fn double_expensive_fn<T: Foo>(foo: &T, x: i64, y: i64) -> i64 {
+//!     foo.expensive_fn(x, y) * 2
+//! }
+//!
+//! fn test_doubles_return_value() {
+//!     let mock = MockFoo { expensive_fn: Mock::default() };
+//!
+//!     mock.expensive_fn.return_value(1000);
+//!
+//!     assert_eq!(double_expensive_fn(&mock, 1, 2), 2000);
+//! }
+//!
+//! fn test_uses_correct_args() {
+//!     let mock = MockFoo { expensive_fn: Mock::default() };
+//!
+//!     assert!(!mock.expensive_fn.called());
+//!
+//!     double_expensive_fn(&mock, 1, 2);
+//!
+//!     assert_eq!(mock.expensive_fn.num_calls(), 1);
+//!     assert!(mock.expensive_fn.called_with((11, 2)));
+//! }
+//!
+//! test_doubles_return_value();
+//! test_uses_correct_args();
+//! ```
+
 pub use mock::Mock;
 
 mod mock;
